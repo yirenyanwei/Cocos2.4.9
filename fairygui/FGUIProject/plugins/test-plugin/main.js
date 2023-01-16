9b97ce2e628bd0ce23b08a77a9f973f93fb26ed1
haoyanwei <haoyanwei@elex-tech.com>
2023-01-16 11:23:55 +0800

    0116

---
+++ fairygui/FGUIProject/plugins/test-plugin/main.js
@@ -0,0 +1,5 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const csharp_1 = require('csharp');
+const App = csharp_1.FairyEditor.App;
+console.log('hello world');
---
+++ fairygui/FGUIProject/plugins/test-plugin/main.ts
@@ -0,0 +1,7 @@
+//FYI: https://github.com/Tencent/puerts/blob/master/doc/unity/manual.md
+
+import { FairyEditor } from 'csharp';
+
+const App = FairyEditor.App;
+
+console.log('hello world');
